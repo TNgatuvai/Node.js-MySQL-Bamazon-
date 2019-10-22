e64ed9dd049fe36dbfd2676e3dde0394aa475583
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-10-22 00:17:45 -0600

    connection with server

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,11 @@ let connection = mysql.createConnection( {
     database: "bamazon_DB",
 
 });
+
+// Creates the connection with the server and loads the product data upon a successful connection
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+    }
+    loadProducts();
+  });
